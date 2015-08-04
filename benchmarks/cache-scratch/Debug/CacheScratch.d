edbb2515586583dd9a527636b962f1be5ec88dea
Archita Agarwal <architaagarwal@Architas-MacBook-Pro.local>
2015-08-04 11:52:23 -0400

    added debug folders

---
+++ benchmarks/cache-scratch/Debug/CacheScratch.d
@@ -0,0 +1,19 @@
+CacheScratch.d: ../CacheScratch.c \
+ ../../../allocators/WaitFreeMemAlloc/src/WaitFreePool.h \
+ ../../../allocators/WaitFreeMemAlloc/src/Block.h \
+ ../../../allocators/WaitFreeMemAlloc/src/commons.h \
+ ../../../allocators/WaitFreeMemAlloc/src/utils.h \
+ ../../../allocators/WaitFreeMemAlloc/src/mini-logger/logger.h \
+ ../../../utils/mini-logger/logger.h
+
+../../../allocators/WaitFreeMemAlloc/src/WaitFreePool.h:
+
+../../../allocators/WaitFreeMemAlloc/src/Block.h:
+
+../../../allocators/WaitFreeMemAlloc/src/commons.h:
+
+../../../allocators/WaitFreeMemAlloc/src/utils.h:
+
+../../../allocators/WaitFreeMemAlloc/src/mini-logger/logger.h:
+
+../../../utils/mini-logger/logger.h:
